ca8ddb014b596ed7690a7e7fc83d93a096de6a90
Ben-cs50 <benedictodhiambo49@gmail.com>
2025-05-21 18:00:28 +0300

    changed port to 3000

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 describe('MO introduction', () => {
   it('MO text exist', () => {
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:3000')
     cy.get('[data-test="MO-header"]').should('exist')
   })
   it('Shows full details of developer profile', () => {
-      cy.visit('http://localhost:3001')
+      cy.visit('http://localhost:3000')
       cy.get('[data-test="About-button"]').click()
       })
 
